4ab2e71a1a2502cc7133179c41f436af39db377f
matthis-guilleray <mat.guilleray@gmail.com>
2023-11-22 17:37:54 +0100

    Adding the beamng path as Env Var Bug Fix : The beamng instance was closing

--- launch_beamng.tech.py
+++ launch_beamng.tech.py
@@ -1,4 +1,12 @@
 from beamngpy import BeamNGpy
+import time
+import os
 
-beamng = BeamNGpy('localhost', 64256, home = "C:/Users/MSI/Desktop/BeamNG.tech.v0.30.6.0", quit_on_close=False, crash_lua_on_error=False)
+beamng_tech_home = os.getenv('BEAMNG_TECH_HOME')
+if beamng_tech_home is None:
+    raise EnvironmentError("BEAMNG_TECH_HOME environment variable is not defined")
+
+beamng = BeamNGpy('localhost', 64256, home = beamng_tech_home, quit_on_close=False, crash_lua_on_error=False)
 beamng.open(launch=True, listen_ip="*")
+while True:
+    time.sleep(10)
